55f24442341ca1179c680c6f73b4f83161c249aa
Nick Chapman <nic@amadido.co.uk>
2024-02-08 13:08:17 +0000

    implement unit-prop: 1/10s timeouts: 47 -> 34

--- src/Solve.hs
+++ src/Solve.hs
@@ -4,7 +4,9 @@ module Solve
   , Answer(..)
   ) where
 
+import Data.List (intercalate)
 import Spec (Spec(..),Clause(..),Literal(..))
+--import Text.Printf (printf)
 
 data Answer = UnSat | Sat [Literal]
 
@@ -14,20 +16,66 @@ instance Show Answer where
     Sat xs -> "v " ++ show (Clause xs)
 
 solve :: Spec -> IO Answer
-solve Spec{clauses=clauses0} = go (initState clauses0)
+solve Spec{clauses=clauses0} = top
   where
-    go :: State -> IO Answer
-    go s = do
-      case areWeDone s of
-        Just a -> do
-          pure a
+
+    doUnitProp = True
+
+    -- TODO: keep track of the number of decisions we make,and how any conflicts we run into
+
+    top :: IO Answer
+    top = do
+      (_n,res) <- go 0 (initState clauses0)
+      --printf "#steps=%d\n" _n
+      pure res
+
+    maybeUnitProp = if doUnitProp then tryUnitProp else Just
+
+    go :: Int -> State -> IO (Int,Answer)
+    go n0 s0 = do
+      let n = 1 + n0
+      --print (n,s0)
+      case maybeUnitProp s0 of
         Nothing -> do
-          let x = pickLit s
-          go (extendAss x s) >>= \case
-            a@Sat{} -> do
-              pure a
-            UnSat -> do
-              go (extendAss (inverse x) s)
+          pure (n,UnSat) -- TODO: dont use answer for these partial results
+        Just s -> do
+          case areWeDone s of
+            Just a -> do
+              pure (n, a)
+            Nothing -> do
+              let x = pickLit s
+              go n (extendAssNeverInvalid x s) >>= \case
+                a@(_,Sat{}) -> do
+                  pure a
+                (n,UnSat) -> do
+                  go n (extendAssNeverInvalid (inverse x) s)
+
+
+-- because we made the decision only for a var not already decided
+extendAssNeverInvalid :: Literal -> State -> State
+extendAssNeverInvalid x s =
+  case extendAss x s of
+    Nothing -> error "invalid extension"
+    Just s -> s
+
+
+tryUnitProp :: State -> Maybe State
+tryUnitProp s = do
+  case unitClauseLits s of
+    [] -> Just s
+    xs -> tryExtends s xs
+  where
+    tryExtends :: State -> [Literal] -> Maybe State
+    tryExtends s = \case
+      [] -> tryUnitProp s
+      x:xs -> do
+        case extendAss x s of
+          Nothing -> Nothing
+          Just s' -> tryExtends s' xs
+
+unitClauseLits :: State -> [Literal]
+unitClauseLits State{toBeSat} = [ x | Clause [x] <- toBeSat ]
+
 
 inverse :: Literal -> Literal
 inverse = \case Pos x -> Neg x; Neg x -> Pos x
@@ -37,6 +85,8 @@ data State = State { toBeSat :: [Clause], ass :: [Literal] }
 instance Show State where
   show State{toBeSat,ass} =
     "#clauses: " ++ show (length toBeSat) ++ ", ass: " ++ show ass
+    --"ass: " ++ show ass ++ ", clauses: " ++ intercalate "|" (map show toBeSat)
+    where _ = intercalate
 
 initState :: [Clause] -> State
 initState toBeSat = State {toBeSat, ass = []}
@@ -51,12 +101,13 @@ pickLit :: State -> Literal
 pickLit State{toBeSat} =
   head [ lit | Clause lits <- toBeSat, lit <- lits ]
 
-extendAss :: Literal -> State -> State
+
+extendAss :: Literal -> State -> Maybe State
 extendAss x State{toBeSat=clauses0,ass=ass0} =
-  if inverse x `elem` ass0 then error "invalid extension" else
-    State { toBeSat = map cantBeSat (filter (not . nowSat) clauses0)
-          , ass = x : ass0
-          }
+  if inverse x `elem` ass0 then Nothing else
+    Just $ State { toBeSat = map cantBeSat (filter (not . nowSat) clauses0)
+                 , ass = x : ass0
+                 }
       where
         nowSat (Clause xs) = x `elem` xs
         cantBeSat (Clause xs) = Clause (filter (/= inverse x) xs)
